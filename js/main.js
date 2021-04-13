30305d58ff2cf5c4578306b473c11d2b0ae39866
stephenbrooks40 <stephenbrooks714@gmail.com>
2021-04-13 13:18:07 -0700

    update

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var code = ["scss", "html5", "jQuery", "JavaScript"];
+document.getElementById("block").innerHTML = code.toString();
+// console.log(code);
+document.getElementById("style").style.color = "red";
